6839c0531723d9a3e7035f48c5574a27b9dea066
Oyasi Kelly <104142584+Oyasikelly@users.noreply.github.com>
2023-10-25 10:56:05 +0100

    NewUpdate

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+"use strict";
 
+const menueTab = document.querySelector(".menue-tab");
+const desktopMenue = document.querySelector("ul");
+const mobileMenue = document.querySelector(".menue-wrapper");
+const header = document.querySelector("header");
+
+menueTab.addEventListener("click", (e) => {
+  mobileMenue.classList.add("show");
+  menueTab.style.display = "none";
+  header.classList.add("addBlur");
+});
+
+mobileMenue.addEventListener("click", () => {
+  mobileMenue.classList.remove("show");
+  menueTab.style.display = "flex";
+  header.classList.remove("addBlur");
+});
